deb2952091c81b1671c3958e879dad37cf8687dc
leechiwang <leechiwa@usc.edu>
2024-10-03 16:07:43 -0700

    just apply the topic

---
+++ src/data/site-config.ts
@@ -0,0 +1,109 @@
+export type Image = {
+    src: string;
+    alt?: string;
+    caption?: string;
+};
+
+export type Link = {
+    text: string;
+    href: string;
+};
+
+export type Hero = {
+    title?: string;
+    text?: string;
+    image?: Image;
+    actions?: Link[];
+};
+
+export type Subscribe = {
+    title?: string;
+    text?: string;
+    formUrl: string;
+};
+
+export type SiteConfig = {
+    logo?: Image;
+    title: string;
+    subtitle?: string;
+    description: string;
+    image?: Image;
+    headerNavLinks?: Link[];
+    footerNavLinks?: Link[];
+    socialLinks?: Link[];
+    hero?: Hero;
+    subscribe?: Subscribe;
+    postsPerPage?: number;
+    projectsPerPage?: number;
+};
+
+const siteConfig: SiteConfig = {
+    title: 'Lee Chi Wang',
+    subtitle: 'MS in Computer Science at USC, expected graduation: Dec 2025',
+    description: 'Personal blog build by Astro.js and Tailwind CSS theme',
+    // image: {
+    //     src: '/dante-preview.jpg',
+    //     alt: 'Dante - Astro.js and Tailwind CSS theme'
+    // },
+    headerNavLinks: [
+        {
+            text: 'Home',
+            href: '/'
+        },
+        {
+            text: 'Projects',
+            href: '/projects'
+        },
+        {
+            text: 'Blog',
+            href: '/blog'
+        },
+        {
+            text: 'Tags',
+            href: '/tags'
+        }
+    ],
+    footerNavLinks: [
+        {
+            text: 'About',
+            href: '/about'
+        },
+        {
+            text: 'Contact',
+            href: '/contact'
+        }
+    ],
+    socialLinks: [
+        {
+            text: 'Linkedin',
+            href: 'https://www.linkedin.com/in/leechiwang'
+        },
+        {
+            text: 'Instagram',
+            href: 'https://www.instagram.com/alisa_tyys/profilecard/?igsh=MzRIODBiNWFIZA=='
+        }
+    ],
+    hero: {
+        title: 'Hi There, welcome to my little world!',
+        text: 'My name is **Lee-Chi Wang**.',
+        image: {
+            src: '/myself.jpeg',
+            alt: 'me standing in the shrine'
+        },
+        actions: [
+            {
+                text: 'Get in Touch',
+                href: '/contact'
+            }
+        ]
+    },
+    subscribe: {
+        title: 'Subscribe to Dante Newsletter',
+        text: 'One update per week. All the latest posts directly in your inbox.',
+        formUrl: '#'
+    },
+    postsPerPage: 8,
+    projectsPerPage: 8
+};
+
+export default siteConfig;
